467c77c3f5217f0765b1e50131c4ba3ce3f105a8
dan <146228938+deplat@users.noreply.github.com>
2024-06-26 22:38:53 +0300

    CatCard, CatList, Cats: range filter

--- src/components/CatCard.jsx
+++ src/components/CatCard.jsx
@@ -3,12 +3,24 @@ import {Card, Col} from "react-bootstrap";
 const avatarHost = import.meta.env.VITE_SERVER_HOST + '/avatar'
 
 const CatCard = ({cat}) => {
+
+    const imgSrc = (cat) => {
+        return cat.avatar ? `${avatarHost}/${cat.avatar}` : `${avatarHost}/avatar-526f1510-3395-11ef-bb6b-af4a695f6c28.jpg`
+    }
+
     return (
         <Col md={4} lg={3}>
-            <Card className="text-center" style={{padding: "10px"}}>
-                <Card.Img src={`${avatarHost}/${cat.avatar}`} alt={`${avatarHost}/${cat.avatar}`}/>
-                <div>{cat.name}</div>
-                <div>{cat.age}</div>
+            <Card className="text-center" style={{marginBottom: 20}}>
+                <Card.Img
+                    className={"text-center align-content-center"}
+                    src={imgSrc(cat)}
+                    alt={'Фото скоро появится'}
+                    style={{height: "30vh", width: "100%", objectFit: "cover"}}
+                />
+                <Card.Body>
+                    <Card.Title>{cat.name}</Card.Title>
+                    <Card.Subtitle>{cat.age}</Card.Subtitle>
+                </Card.Body>
             </Card>
         </Col>
     );
--- src/pages/Cats.jsx
+++ src/pages/Cats.jsx
@@ -1,16 +1,19 @@
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Form, Row} from "react-bootstrap";
 import CatList from "../components/CatList.jsx";
 
 const Cats = () => {
     return (
         <Container fluid>
-            <Row md={12} className={"d-flex"}>
-                <Col md={3} lg={2}>
-                    <p>SexBar</p>
-                </Col>
-                <Col md={9} lg={10}>
-                    <p>ColorsBar</p>
-                </Col>
+            <Row md={12} style={{padding:"1vh",marginBottom:"1vh",minHeight: "2vh"}}>
+                <Form className={"d-flex"}>
+                    <Col md={3} lg={2} justify={"center"} align={"center"} style={{
+                    }}>
+                        <Form.Range style={{width: "75%"}} value={0}/>
+                    </Col>
+                    <Col md={9} lg={10} align={"center"}>
+                        <p>ColorsBar</p>
+                    </Col>
+                </Form>
             </Row>
             <Row md={12} className={"d-flex"}>
                 <Col lg={12}>
